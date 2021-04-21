a4e47ed5c846c38b135d647163df5b466919a4e7
Brandon DuBois <brandub@gmail.com>
2021-04-20 21:49:13 -0700

    blog modal complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
 $(function(){
-    $("#buttonBlog").click(function(){
+    $("#buttonBlogOne").click(function(){
+        let blogTitleOne = document.querySelector("#blogTitleOne").innerHTML;
+        document.querySelector("#exampleModalLongTitle").innerHTML = blogTitleOne;
+        document.querySelector("#hiddenSpanOne").style.visibility = "visible";
+        let blogTextOne = document.querySelector("#textOne").textContent;
+        document.querySelector("#sharedModalBody").innerHTML = blogTextOne;
+        $("#exampleModalLong").modal("show"); 
+    });
+    $("#buttonBlogTwo").click(function(){
+        let blogTitleTwo = document.querySelector("#blogTitleTwo").innerHTML;
+        document.querySelector("#exampleModalLongTitle").innerHTML = blogTitleTwo;
+        document.querySelector("#hiddenSpanTwo").style.visibility = "visible";
+        let blogTextTwo = document.querySelector("#textTwo").textContent;
+        document.querySelector("#sharedModalBody").innerHTML = blogTextTwo;
+        $("#exampleModalLong").modal("show"); 
+    });
+    $("#buttonBlogThree").click(function(){
         let blogTitle = document.querySelector("#blogTitleThree").innerHTML;
         document.querySelector("#exampleModalLongTitle").innerHTML = blogTitle;
         document.querySelector("#hiddenSpanThree").style.visibility = "visible";
-        let blogText = document.querySelector("#textThree").innerHTML;
-        document.querySelector("#bodyThree").removeClass = "style";
+        let blogText = document.querySelector("#textThree").textContent;
+        document.querySelector("#sharedModalBody").innerHTML = blogText;
         $("#exampleModalLong").modal("show"); 
     });
+    
 });
 
 
