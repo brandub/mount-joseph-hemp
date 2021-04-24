26d8b882e1ba0785e6d7d3b47884488c33e5ba16
Brandon DuBois <brandub@gmail.com>
2021-04-24 13:14:48 -0700

    fixed modal popout removal of example.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 $(function(){
     $("#buttonBlogOne").click(function(){
         let blogTitleOne = document.querySelector("#blogTitleOne").innerHTML;
-        document.querySelector("#exampleModalLongTitle").innerHTML = blogTitleOne;
+        document.querySelector("#ModalLongTitle").innerHTML = blogTitleOne;
         document.querySelector("#hiddenSpanOne").style.visibility = "visible";
         let blogTextOne = document.querySelector("#textOne").textContent;
         document.querySelector("#sharedModalBody").innerHTML = blogTextOne;
-        $("#exampleModalLong").modal("show"); 
+        $("#ModalLong").modal("show"); 
     });
     $("#buttonBlogTwo").click(function(){
         let blogTitleTwo = document.querySelector("#blogTitleTwo").innerHTML;
-        document.querySelector("#exampleModalLongTitle").innerHTML = blogTitleTwo;
+        document.querySelector("#ModalLongTitle").innerHTML = blogTitleTwo;
         document.querySelector("#hiddenSpanTwo").style.visibility = "visible";
         let blogTextTwo = document.querySelector("#textTwo").textContent;
         document.querySelector("#sharedModalBody").innerHTML = blogTextTwo;
-        $("#exampleModalLong").modal("show"); 
+        $("#ModalLong").modal("show"); 
     });
     $("#buttonBlogThree").click(function(){
         let blogTitle = document.querySelector("#blogTitleThree").innerHTML;
-        document.querySelector("#exampleModalLongTitle").innerHTML = blogTitle;
+        document.querySelector("#ModalLongTitle").innerHTML = blogTitle;
         document.querySelector("#hiddenSpanThree").style.visibility = "visible";
         let blogText = document.querySelector("#textThree").textContent;
         document.querySelector("#sharedModalBody").innerHTML = blogText;
-        $("#exampleModalLong").modal("show"); 
+        $("#ModalLong").modal("show"); 
     });
     $("#subscribeButton").click(function(){
         document.querySelector("#emailInput").value = "Thank you!";
@@ -32,7 +32,7 @@ $(function(){
         let checkOut = document.querySelector("#checkOut").value;
         let guestName = document.querySelector("#guestName").value;
         let siteSelection =document.querySelector("#siteSelection").value;
-        $("#exampleModal").modal("toggle");
+        $("#Modal").modal("toggle");
         alert("Thank you " + guestName + " for booking " + siteSelection + " Checking in on " + checkIn + " Checking out on " + checkOut + " You will receive a confirmation email shortly");
     });
     
